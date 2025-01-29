d7d4c8aaa3b9df5b4b83bd44616fd29ce954dcb5
Daniel Oyagha <daniel.oyagha@gmail.com>
2025-01-29 14:34:13 +0100

    --initialize a new project using spring.io --added basic git ignore files extensions

---
+++ src/main/java/com/hng/task1/demo/controllers/BasicInformationController.java
@@ -0,0 +1,23 @@
+package com.hng.task1.demo.controllers;
+
+import com.hng.task1.demo.models.BasicInformationDTO;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class BasicInformationController {
+  @GetMapping
+  public BasicInformationDTO getBasicInformation() {
+
+    return new BasicInformationDTO(
+        "yagzy@gmail.com",
+        DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
+        "https://github.com/dyagzy");
+  }
+}
---
+++ src/main/java/com/hng/task1/demo/models/BasicInformationDTO.java
@@ -0,0 +1,10 @@
+package com.hng.task1.demo.models;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BasicInformationDTO {
+  private String email;
+  private String current_dateTime;
+  private String github_url;
+}
