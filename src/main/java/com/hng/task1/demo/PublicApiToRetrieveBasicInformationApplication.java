7ac2b55043a60eb728c728a8c266e2e5e1cbb5dd
Daniel Oyagha <daniel.oyagha@gmail.com>
2025-01-29 14:34:23 +0100

    --initialize a new project using spring.io --added basic git ignore files extensions

---
+++ src/main/java/com/hng/task1/demo/PublicApiToRetrieveBasicInformationApplication.java
@@ -0,0 +1,13 @@
+package com.hng.task1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicApiToRetrieveBasicInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicApiToRetrieveBasicInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hng/task1/demo/PublicApiToRetrieveBasicInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng.task1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicApiToRetrieveBasicInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
